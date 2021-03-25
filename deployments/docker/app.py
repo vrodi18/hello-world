cc87b24012f15da9409c4967d9b95bfd4d35c7be
Vladlen Rodionov <72477919+vrodi18@users.noreply.github.com>
2021-03-25 15:53:26 -0500

    Update app.py

--- deployments/docker/app.py
+++ deployments/docker/app.py
@@ -1,12 +1,19 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask,  jsonify 
+import os 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return jsonify({
-        'message': 'Hello World',
-        'environment': os.environ.get('ENVIRONMENT')
+        'message': 'hello world',
+        'environment': os.environ.get('ENVIRONMENT'),
+        'namespace': os.environ.get('NAMESPACE')
+    })
+
+@app.route('/soon')
+def comming_soon():
+    return jsonify({
+        'message': 'This is comming soon page!!'
     })
 
 if __name__ == '__main__':
