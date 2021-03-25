daf52aeb6894c3681ed2acae8dc4e26434b087b5
Vladlen Rodionov <72477919+vrodi18@users.noreply.github.com>
2021-03-25 16:08:40 -0500

    Update main.tf

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,42 +1,12 @@
-module "helm_deploy" {
-  source                    = "fuchicorp/chart/helm"
-  deployment_name           = "${var.deployment_name}"
-  deployment_environment    = "${var.deployment_environment}"
-  deployment_endpoint       = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
-  deployment_path           = "hello-world"     
-  template_custom_vars = {
-    deployment_image        = "${var.deployment_image}"
-  }          
-}
-
-variable "deployment_environment" {
-    default = "prod"
-    description = "- (Optional) The namespace to deploy the helm chart."
-}
-
-variable "deployment_name" {
-    default = "hello-world"
-    description = "- (Optional) The name of the deployment."
-}
-
-variable "deployment_image" {
-    default = "docker.fuchicorp.com/hello-world-stage:c6e96b5"
-    description = "- (Optional) The docker image of the deployment."
-}
-
-variable "deployment_endpoint" {
-    type = "map"
-
-    default = {
-        dev     = "dev.hello"
-        qa      = "qa.hello"
-        prod    = "hello"
-        stage   = "stage.hello"
-    }
-
-    description = "- (Optional) The endpooint of the deployment."
-}
-
-variable "google_domain_name" {
-  default = "fuchicorp.com"
+module "academy-deploy" {
+  source  = "tuyalou/chart/helm"
+  deployment_name        = "hello-world"  deployment_environment = "${var.deployment_environment}"
+  deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
+  deployment_path        = "hello-world"
+  template_custom_vars  = {
+    deployment_image     = "${var.deployment_image}"
+  }
+} 
+output "application_endpoint" {
+    value = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
 }
